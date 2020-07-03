04856d3b0e2994f5ee49267775dfc64673db8174
serii <seriiburduja@gmail.com>
2020-07-03 09:59:22 +0300

    scss

--- gulpfile.js
+++ gulpfile.js
@@ -12,35 +12,43 @@ let gulp = require('gulp'),
 	browserSync = require('browser-sync').create(),
 	replace = require('gulp-replace');
 
-// gulp.task("css", function () {
-// 	return gulp.src('../wc-estore/assets/sass/my.scss')
-// 		.pipe(plumber())
-// 		.pipe(sourcemaps.init())
-// 		.pipe(wait(500))
-// 		.pipe(sass({
-// 			outputStyle: 'expanded'
-// 		}).on('error', notify.onError(function (error) {
-// 			return 'An error occurred while compiling sass.\nLook in the console for details.\n' + error;
-// 		})))
-// 		.pipe(autoprefixer({
-// 			cascade: false
-// 		}))
-// 		.pipe(sourcemaps.write('.'))
-// 		.pipe(gulp.dest('../wc-estore/assets/css/'))
-// 		.pipe(browserSync.reload({
-// 			stream: true
-// 		}));
-// 	// .pipe(notify("Change css"));
-// });
+let siteUrl = 'http://wc-estore.host1670806.hostland.pro/';
+let siteDir = '../wc-estore/';
+
+gulp.task("scss", function () {
+	return gulp.src(siteDir + 'assets/scss/my.scss')
+		// .pipe(plumber())
+		.pipe(sourcemaps.init())
+		.pipe(wait(500))
+		.pipe(sass({
+			outputStyle: 'expanded'
+		}).on('error', notify.onError(function (error) {
+			return 'An error occurred while compiling sass.\nLook in the console for details.\n' + error;
+		})))
+		.pipe(autoprefixer({
+			cascade: false
+		}))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest(siteDir + 'assets/css/'))
+		.pipe(browserSync.reload({stream: true}))
+		.pipe(notify("Change css"));
+});
+
+gulp.task("watch", function () {
+	gulp.watch(siteDir + 'assets/scss/**/*.scss', gulp.series('scss'));
+});
 
 gulp.task('browser-sync', function () {
 	browserSync.init({
-		proxy: "http://wc-estore.host1670806.hostland.pro/",
-        reloadDelay: 2000
+		proxy: {
+			target: siteUrl,
+			ws: true
+		},
+		reloadDelay: 2000
 	});
-	gulp.watch("../wc-estore/**/*.php").on('change', browserSync.reload);
-	gulp.watch("../wc-estore/**/*.css").on('change', browserSync.reload);
-	gulp.watch("../wc-estore/**/*.js").on('change', browserSync.reload);
+	gulp.watch(siteDir + "**/*.php").on('change', browserSync.reload);
+	gulp.watch(siteDir + "**/*.css").on('change', browserSync.reload);
+	gulp.watch(siteDir + "**/*.js").on('change', browserSync.reload);
 });
 
-gulp.task('default', gulp.parallel('browser-sync') );
+gulp.task('default', gulp.parallel('watch', 'browser-sync'));
