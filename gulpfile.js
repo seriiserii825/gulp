83e5c302f7b65951f0be24d09459eff020d02215
serii <seriiburduja@gmail.com>
2020-08-03 10:47:01 +0300

    gadda

--- gulpfile.js
+++ gulpfile.js
@@ -18,8 +18,8 @@ let gulp = require('gulp'),
 // let siteUrl = 'http://wc-estore.host1670806.hostland.pro/';
 // let siteDir = '../wc-estore/';
 
-// let siteUrl = 'http://gadda.cf/';
-// let siteDir = '../bs-gadda/';
+let siteUrl = 'http://gadda.cf/';
+let siteDir = '../bs-gadda/';
 
 // let siteUrl = 'http://dev.ilcaffecheaspettavi.it/';
 // let siteDir = '../bs-alytaly/';
@@ -27,8 +27,8 @@ let gulp = require('gulp'),
 // let siteUrl = 'https://myrewind.it/';
 // let siteDir = '../bs-rewind/';
 
-let siteUrl = 'http://bertan.ml/';
-let siteDir = '../bs-bertan/';
+// let siteUrl = 'http://bertan.ml/';
+// let siteDir = '../bs-bertan/';
 
 //let siteUrl = 'http://proseccoborgoluce.cf//';
 //let siteDir = '../bs-proseccobordoluce/';
