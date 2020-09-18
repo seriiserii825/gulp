654a9ad29d3f217f3a422d9807e02e36f16822e0
serii <seriiburduja@gmail.com>
2020-09-18 11:39:42 +0300

    zuccato

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,7 @@ let gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	autoprefixer = require("gulp-autoprefixer"),
 	sourcemaps = require('gulp-sourcemaps'),
-	wait = require('gulp-wait'),
-	notify = require("gulp-notify"),
+	wait = require('gulp-wait'), notify = require("gulp-notify"),
 	plumber = require("gulp-plumber"),
 	browserSync = require('browser-sync').create(),
 	replace = require('gulp-replace');
@@ -25,10 +24,13 @@ let gulp = require('gulp'),
 // let siteDir = '../vue/';
 
 // let siteDir = '../gsap/';
-let siteDir = '../javascript-petricenco/';
+// let siteDir = '../javascript-petricenco/';
 
 // let siteUrl = 'http://javascript.host1670806.hostland.pro/';
 // let siteDir = '../javascript-petricenco/';
+
+let siteUrl = 'http://zuccato.cf/';
+let siteDir = '../bs-zuccato/';
 
 //let siteDir = '../js-movies/';
 
@@ -51,63 +53,49 @@ let siteDir = '../javascript-petricenco/';
 //let siteDir = '../bs-proseccobordoluce/';
 
 
-// gulp.task("scss", function () {
-// 	return gulp.src(siteDir + 'assets/scss/my.scss')
-// 		// .pipe(plumber())
-// 		.pipe(sourcemaps.init())
-// 		.pipe(wait(500))
-// 		.pipe(sass({
-// 			outputStyle: 'expanded'
-// 		}).on('error', notify.onError(function (error) {
-// 			return 'An error occurred while compiling sass.\nLook in the console for details.\n' + error;
-// 		})))
-// 		.pipe(autoprefixer({
-// 			cascade: false
-// 		}))
-// 		.pipe(sourcemaps.write('.'))
-// 		.pipe(gulp.dest(siteDir + 'assets/css/'))
-// 		.pipe(browserSync.reload({stream: true}));
-// });
+gulp.task("scss", function () {
+	return gulp.src(siteDir + 'assets/scss/my.scss')
+		// .pipe(plumber())
+		.pipe(sourcemaps.init())
+		.pipe(wait(500))
+		.pipe(sass({
+			outputStyle: 'expanded'
+		}).on('error', notify.onError(function (error) {
+			return 'An error occurred while compiling sass.\nLook in the console for details.\n' + error;
+		})))
+		.pipe(autoprefixer({
+			cascade: false
+		}))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest(siteDir + 'assets/css/'))
+		.pipe(browserSync.reload({stream: true}));
+});
 
 
-// gulp.task("watch", function () {
-// 	gulp.watch(siteDir + 'assets/scss/**/*.scss', gulp.series('scss'));
-// });
+gulp.task("watch", function () {
+	gulp.watch(siteDir + 'assets/scss/**/*.scss', gulp.series('scss'));
+});
 
 gulp.task('browser-sync', function () {
-	// browserSync.init({
-	// 	proxy: {
-	// 		target: siteUrl,
-	// 		ws: true
-	// 	},
-	// 	reloadDelay: 1500
-	// });
-
 	browserSync.init({
-		server: {
-			baseDir: siteDir
+		proxy: {
+			target: siteUrl,
+			ws: true
 		},
-		notify: true
+		reloadDelay: 1500
 	});
+
+	// browserSync.init({
+	// 	server: {
+	// 		baseDir: siteDir
+	// 	},
+	// 	notify: true
+	// });
 	gulp.watch(siteDir + "**/*.html").on('change', browserSync.reload);
 	gulp.watch(siteDir + "**/*.php").on('change', browserSync.reload);
 	gulp.watch(siteDir + "**/*.css").on('change', browserSync.reload);
 	gulp.watch(siteDir + "**/*.js").on('change', browserSync.reload);
 });
 
-// gulp.task('browser-sync', function () {
-
-// 	browserSync.init({
-// 		server: {
-// 			baseDir: siteDir
-// 		},
-// 		notify: true
-// 	});
-// 	gulp.watch(siteDir + "**/*.php").on('change', browserSync.reload);
-// 	gulp.watch(siteDir + "**/*.css").on('change', browserSync.reload);
-// 	gulp.watch(siteDir + "**/*.js").on('change', browserSync.reload);
-// 	gulp.watch(siteDir + "**/*.html").on('change', browserSync.reload);
-// });
-
-gulp.task('default', gulp.series('browser-sync'));
-// gulp.task('default', gulp.parallel('watch', 'browser-sync'));
+// gulp.task('default', gulp.series('browser-sync'));
+gulp.task('default', gulp.parallel('watch', 'browser-sync'));
